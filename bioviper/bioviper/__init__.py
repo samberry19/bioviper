3646912849bc3088cc102cdf9385d665199a1dc6
Sam Berry <61884003+samberry19@users.noreply.github.com>
2022-08-02 10:37:56 -0400

    Delete bioviper directory

--- bioviper/bioviper/__init__.py
+++
@@ -1,7 +0,0 @@
-from .msa import MultipleSequenceAlignment, SequenceArray
-from .msa import readAlignment, readSequences, PairwiseAlign, CalcIdentityMatrix, cluster_sizes
-
-from .pdb import ProteinStructure
-from .pdb import readPDB, readAlphaFold, CalcDistanceMatrix
-
-#from .TreeBuilders import NJTree, UPGMATree, MuscleAlign, FastTree
