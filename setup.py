ba94605af88303d71e7793826830304e4eaf6abd
Sam Berry <61884003+samberry19@users.noreply.github.com>
2025-02-03 19:02:45 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='bioviper',
       author_email="sberry@g.harvard.edu",
       description = "Enhancements to Biopython for working with biological data",
       py_modules=["bioviper", "bioviper.msa", "bioviper.pdb", "bioviper.hmmer_tools",
-                  "bioviper.phylo"],
+                  "bioviper.phylo", "bioviper.utils"],
       install_requires=[
         'numpy',
         'pandas',
